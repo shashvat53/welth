2d88f567ea5defee134046a84432fc60c0ad38c3
sha1sha2 <shashvatsen00@gmail.com>
2025-06-29 01:54:46 +0530

    integrate inngest, react-email, resend for cron job functionality

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,13 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
+import { checkBudgetAlerts } from "@/lib/inngest/functions";
+import { inngest } from "@/lib/inngest/client";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
+    checkBudgetAlerts,
+    // helloWorld,
   ],
 });
